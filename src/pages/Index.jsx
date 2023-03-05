876a31c3c877b157fa7b1685a21c7365bf573c07
Etienne Bellé <bl.etienne7@gmail.com>
2023-03-05 16:54:39 +0100

    set up react app

---
+++ src/pages/Index.jsx
@@ -0,0 +1,14 @@
+import React, { useState, useEffect } from 'react'
+
+function Index() {
+
+    const [articles, setArticles] = useState([]);
+
+    return (
+        <div>
+            <h1 className='app__title'>Hello</h1>
+        </div>
+    )
+}
+
+export default Index
